bef3b1ce3158af426dcfda275b4303348fb35ee7
Christine Suson <christinesuson@Christines-MacBook-Pro.local>
2020-10-01 15:40:19 +0800

    Updates

---
+++ precache-manifest.a5b95ca167a1d0fb39580c313400e5bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2bb1a744876f903669fac1166fea004",
+    "url": "/RecipeApp/index.html"
+  },
+  {
+    "revision": "649dded6d181b6409e2c",
+    "url": "/RecipeApp/static/css/main.3454a6c6.chunk.css"
+  },
+  {
+    "revision": "f6683532279832adc15f",
+    "url": "/RecipeApp/static/js/2.a8ee570a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RecipeApp/static/js/2.a8ee570a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "649dded6d181b6409e2c",
+    "url": "/RecipeApp/static/js/main.3c4015e3.chunk.js"
+  },
+  {
+    "revision": "db1386236e9e8bdc3314",
+    "url": "/RecipeApp/static/js/runtime-main.aa5bbe4b.js"
+  }
+]);
